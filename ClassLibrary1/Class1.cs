ed51a922ceb70fd69216ed5d7901133ced6e1493
Wonil Park <wpark912@gmail.com>
2019-02-21 21:32:37 -0500

    Ver 1.0 Complete

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class CircleCalculations
+    {
+ 
+        public double Circumference(double value)
+        {
+            return 2 * Math.PI * value;
+
+        }
+
+        public double Area(double value)
+        {
+            return Math.PI * value * value;
+
+        }
+
+        public double Volume(double value)
+        {
+            return ((4.0/3.0) * Math.PI * Math.Pow(value,3)) / 2.0;
+
+        }
+            
+    }
+
+    public class TriangleCalculations
+    {
+            //p = (1 / 2) * (a + b + c);
+            //triarea = Math.Sqrt(p* (p - a) * (p - b) * (p - c));
+    }
+}
--- Wonil_EX1A/Program.cs
+++ Wonil_EX1A/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,97 @@ namespace Wonil_EX1A
     {
         static void Main(string[] args)
         {
+            CircleCalculations circlecalculation = new CircleCalculations();
+
+            double radius;
+
+            Console.Write("Circumference Calculation. Enter radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            if (radius < 0)
+            {
+                Console.WriteLine("Radius cannot be a negative value.");
+            }
+
+            else
+            {
+                Console.WriteLine($"The circumference of circle is: {circlecalculation.Circumference(radius)}");
+
+
+                Console.Write("Area Calculation. Enter radius: ");
+                radius = Convert.ToDouble(Console.ReadLine());
+
+                    if (radius < 0)
+                    {
+                        Console.WriteLine("Radius cannot be a negative value.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"The area of circle is: {circlecalculation.Area(radius)}");
+                    }
+            }
+            
+            Console.Write("Volume Calculation. Enter radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            if (radius < 0)
+            {
+                Console.WriteLine("Radius cannot be a negative value.");
+            }
+            else
+            {
+                Console.WriteLine($"The volume of hemisphere is: {circlecalculation.Volume(radius)}");
+            }
+
+            // Area of a triangle
+            //TriangleCalculations trianglecalculations = new TriangleCalculations();
+
+            double a, b, c, p, triarea;
+
+            Console.Write("Enter the length of first side: ");
+            //a = Convert.ToDouble(Console.ReadLine());
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the length of second side: ");
+            //b = Convert.ToDouble(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the length of third side: ");
+            //c = Convert.ToDouble(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+
+            p = (a + b + c)/2;
+
+            triarea = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            Console.WriteLine($"The area of triangle is: {triarea}");
+
+
+            // Solving a quadratic equation
+            double coef1, coef2, coef3, ans1, ans2, sqrt;
+
+            Console.WriteLine("ax^2 + bx + c = 0.");
+            Console.Write("Enter a: ");
+            coef1 = double.Parse (Console.ReadLine());
+
+            Console.Write("Enter b: ");
+            coef2 = double.Parse (Console.ReadLine());
+
+            Console.Write("Enter c: ");
+            coef3 = double.Parse (Console.ReadLine());
+
+            sqrt = Math.Sqrt(Math.Pow(coef2, 2) - 4 * coef1 * coef3);
+            ans1 = (-1 * coef2 + sqrt) / (2 * coef1);
+            ans2 = (-1 * coef2 - sqrt) / (2 * coef1);
+
+            if (sqrt >= 0)
+            {
+                Console.WriteLine($"The value of x is: {ans1}, {ans2}");
+            }
+            else
+            {
+                Console.WriteLine("You cannot take the square root of a negative number!");
+            }
         }
     }
 }
